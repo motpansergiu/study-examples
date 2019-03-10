d23bee8acec4032b987499ec2283ab3153852852
Sergiu Motpan <motpansergiu@gmail.com>
2019-03-10 21:26:34 +0200

    Add favorite color examples, to understand basic kafka streams functions

---
+++ kafka-streams-samples/favorite-color/src/main/java/dvl/srg/kafka/streams/FavoriteColor.java
@@ -0,0 +1,71 @@
+package dvl.srg.kafka.streams;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+public class FavoriteColor {
+
+    public static final String INPUT_TOPIC = "user-color-input-topic";
+    public static final String OUTPUT_TOPIC = "grouped-color-output-topic";
+
+    static final Set<String> colorCache = new HashSet<>();
+    public static final String USER_COLOR_COMPACTED_TOPIC = "user-color-compacted-topic";
+
+    static {
+        colorCache.add("green");
+        colorCache.add("red");
+        colorCache.add("blue");
+    }
+
+
+
+    public static void main(String[] args) {
+        // Connection properties
+
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "favorite-color");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+
+        KStream<String, String> stream = builder.stream(INPUT_TOPIC);
+
+        KStream<String, String> exctractedByKey = stream.mapValues((ValueMapper<String, String>) String::toLowerCase)
+                .filter((key, value) -> value.contains(","))
+                .selectKey((key, value) -> value.substring(0, value.indexOf(',')))
+                .mapValues(line -> line.substring(line.indexOf(',') + 1))
+                .filter((key, value) -> colorCache.contains(value));
+
+        exctractedByKey.to(USER_COLOR_COMPACTED_TOPIC);
+
+        final Serde<String> stringSerde = Serdes.String();
+        final Serde<Long> longSerde = Serdes.Long();
+
+        KTable<String, Long> count = builder.stream(USER_COLOR_COMPACTED_TOPIC, Consumed.with(stringSerde, stringSerde))
+                .selectKey((key, value) -> value)
+                .groupByKey()
+                .count();
+
+        count.toStream().to(OUTPUT_TOPIC, Produced.with(stringSerde, longSerde));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+    }
+}
