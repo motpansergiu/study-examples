6de4a08cd4d065e3bae8d4ebc31101e40278c23b
Sergiu Motpan <motpansergiu@gmail.com>
2019-03-11 20:51:28 +0200

    Add unit tests for word count app

--- kafka-streams-samples/word-count/src/main/java/dvl/srg/kafka/streams/App.java
+++ kafka-streams-samples/word-count/src/main/java/dvl/srg/kafka/streams/App.java
@@ -6,6 +6,7 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
@@ -15,13 +16,38 @@ import java.util.Arrays;
 import java.util.Properties;
 
 public class App {
+
+    public static final String WORD_COUNT_INPUTT_TOPIC = "word-count-inputt";
+    public static final String WORD_COUNT_OUTPUTT_TOPIC = "word-count-outputt";
+
     public static void main(String[] args) {
         final Properties config = getAppProperties();
 
+        App app = new App();
+
+        KafkaStreams streams = new KafkaStreams(app.createTopology(), config);
+        streams.start();
+
+        // shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+        // Update:
+        // print the topology every 10 seconds for learning purposes
+        while(true){
+            streams.localThreadsMetadata().forEach(data -> System.out.println(data));
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+    public Topology createTopology() {
         StreamsBuilder builder = new StreamsBuilder();
         // 1 - stream from Kafka
 
-        KStream<String, String> textLines = builder.stream("word-count-inputt");
+        KStream<String, String> textLines = builder.stream(WORD_COUNT_INPUTT_TOPIC);
         KTable<String, Long> wordCounts = textLines
                 // 2 - map values to lowercase
                 .mapValues((ValueMapper<String, String>) String::toLowerCase)
@@ -40,25 +66,8 @@ public class App {
         final Serde<Long> longSerde = Serdes.Long();
 
         // 7 - to in order to write the results back to kafka
-        wordCounts.toStream().to( "word-count-outputt", Produced.with(stringSerde, longSerde));
-
-        KafkaStreams streams = new KafkaStreams(builder.build(), config);
-        streams.start();
-
-
-        // shutdown hook to correctly close the streams application
-        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-
-//        // Update:
-//        // print the topology every 10 seconds for learning purposes
-//        while(true){
-//            System.out.println(streams.state());
-//            try {
-//                Thread.sleep(5000);
-//            } catch (InterruptedException e) {
-//                break;
-//            }
-//        }
+        wordCounts.toStream().to(WORD_COUNT_OUTPUTT_TOPIC, Produced.with(stringSerde, longSerde));
+        return builder.build();
     }
 
     private static Properties getAppProperties() {
---
+++ kafka-streams-samples/word-count/src/test/java/dvl/srg/kafka/streams/AppTest.java
@@ -0,0 +1,82 @@
+package dvl.srg.kafka.streams;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.apache.kafka.streams.test.ConsumerRecordFactory;
+import org.apache.kafka.streams.test.OutputVerifier;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    TopologyTestDriver testDriver;
+
+    StringSerializer stringSerializer = new StringSerializer();
+
+    ConsumerRecordFactory<String, String> recordFactory =
+            new ConsumerRecordFactory<>(stringSerializer, stringSerializer);
+
+    @BeforeEach
+    public void setUpTopologyTestDriver(){
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        App wordCountApp = new App();
+        Topology topology = wordCountApp.createTopology();
+        testDriver = new TopologyTestDriver(topology, config);
+    }
+
+    @AfterEach
+    public void closeTestDriver(){
+        testDriver.close();
+    }
+
+    public void pushNewInputRecord(String value){
+        testDriver.pipeInput(recordFactory.create(App.WORD_COUNT_INPUTT_TOPIC, null, value));
+    }
+
+    public ProducerRecord<String, Long> readOutput(){
+        return testDriver.readOutput(App.WORD_COUNT_OUTPUTT_TOPIC, new StringDeserializer(), new LongDeserializer());
+    }
+
+    @Test
+    public void makeSureCountsAreCorrect(){
+        String firstExample = "testing Kafka Streams";
+        pushNewInputRecord(firstExample);
+        OutputVerifier.compareKeyValue(readOutput(), "testing", 1L);
+        OutputVerifier.compareKeyValue(readOutput(), "kafka", 1L);
+        OutputVerifier.compareKeyValue(readOutput(), "streams", 1L);
+        assertEquals(readOutput(), null);
+
+        String secondExample = "testing Kafka again";
+        pushNewInputRecord(secondExample);
+        OutputVerifier.compareKeyValue(readOutput(), "testing", 2L);
+        OutputVerifier.compareKeyValue(readOutput(), "kafka", 2L);
+        OutputVerifier.compareKeyValue(readOutput(), "again", 1L);
+
+    }
+
+    @Test
+    public void makeSureWordsBecomeLowercase(){
+        String upperCaseString = "KAFKA kafka Kafka";
+        pushNewInputRecord(upperCaseString);
+        OutputVerifier.compareKeyValue(readOutput(), "kafka", 1L);
+        OutputVerifier.compareKeyValue(readOutput(), "kafka", 2L);
+        OutputVerifier.compareKeyValue(readOutput(), "kafka", 3L);
+
+    }
+}
