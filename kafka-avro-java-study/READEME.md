02ab9a81083ed98f36829bf802756686bf3508aa
Sergiu Motpan <motpansergiu@gmail.com>
2019-04-08 23:28:29 +0300

    Add generic record and specific record example

---
+++ kafka-avro-java-study/READEME.md
@@ -0,0 +1,7 @@
+avro-tool for reading the avro files
+
+Linux
+$ wget http://repo1.maven.org/maven2/org/apache/avro/avro-tools/1.8.2/avro-tools-1.8.2.jar
+
+Mac
+curl -O http://repo1.maven.org/maven2/org/apache/avro/avro-tools/1.8.2/avro-tools-1.8.2.jar
--- kafka-avro-java-study/src/main/java/dvl/srg/avro/genericrecord/GenericRecordExamles.java
+++ kafka-avro-java-study/src/main/java/dvl/srg/avro/genericrecord/GenericRecordExamles.java
@@ -1,8 +1,14 @@
 package dvl.srg.avro.genericrecord;
 
 import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.*;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+
+import java.io.File;
+import java.io.IOException;
 
 public class GenericRecordExamles {
 
@@ -19,7 +25,7 @@ public class GenericRecordExamles {
                 "        { \"name\": \"last_name\", \"type\" : \"string\" , \"doc\" : \"Last Name of Customer\" },\n" +
                 "        { \"name\": \"age\", \"type\" : \"int\" , \"doc\" : \"Age of Customer\" },\n" +
                 "        { \"name\": \"height\", \"type\" : \"float\" , \"doc\" : \"Height at the time of registration in cm\" },\n" +
-                "        { \"name\": \"weight\", \"type\" : \"float\" , \"doc\" : \"Weight at the time of registration in cm\" },\n" +
+                "        { \"name\": \"weight\", \"type\" : \"float\" , \"doc\" : \"Weight at the time of registration in kg\" },\n" +
                 "        { \"name\": \"automed_email\", \"type\" : \"boolean\" , \"default\" : true, \"doc\" : \"Field indication if the customer want receive emails\" }\n" +
                 "    ]\n" +
                 "}");
@@ -30,8 +36,8 @@ public class GenericRecordExamles {
         builder.set("first_name", "Jon");
         builder.set("last_name", "Snow");
         builder.set("age", 25);
-        builder.set("height", 175.2);
-        builder.set("weight", 70.5);
+        builder.set("height", 175.2f);
+        builder.set("weight", 70.5f);
         builder.set("automed_email", false);
 
         GenericData.Record customer = builder.build();
@@ -42,13 +48,46 @@ public class GenericRecordExamles {
         builderWithDefault.set("first_name", "Jon");
         builderWithDefault.set("last_name", "Snow");
         builderWithDefault.set("age", 25);
-        builderWithDefault.set("height", 175.2);
-        builderWithDefault.set("weight", 70.5);
+        builderWithDefault.set("height", 175.2f);
+        builderWithDefault.set("weight", 70.5f);
 
         GenericData.Record customerWithDefault = builderWithDefault.build();
         System.out.println(customerWithDefault);
 
 
+
+        // step 2 write an avro object to a file
+        final DatumWriter<GenericRecord> writer = new GenericDatumWriter<>(schema);
+        try (DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(writer)) {
+            dataFileWriter.create(customer.getSchema(), new File("customer-generic.avro"));
+            dataFileWriter.append(customer);
+            System.out.println("Written generic customer avro");
+        } catch (IOException e) {
+            System.out.println("Couldn't write file");
+            e.printStackTrace();
+        }
+
+
+        // step 3 read an avro object from a file
+        final File file = new File("customer-generic.avro");
+        final DatumReader<GenericRecord> reader = new GenericDatumReader<>(schema);
+
+        try (DataFileReader<GenericRecord> dataFileWriter = new DataFileReader<>(file, reader)) {
+            GenericRecord readCustomer = dataFileWriter.next();
+            System.out.println("Successfully read avro");
+            System.out.println(readCustomer.toString());
+
+            Object firstName = readCustomer.get("first_name");
+            System.out.println(firstName);
+
+            Object none = readCustomer.get("none");
+
+            System.out.println(none);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
