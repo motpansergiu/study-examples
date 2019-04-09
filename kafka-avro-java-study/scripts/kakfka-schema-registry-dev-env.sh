2754292fcc98d0de1319cc8d7371b3cf7ae2b40d
Sergiu Motpan <motpansergiu@gmail.com>
2019-04-09 21:55:38 +0300

    Add kafka-registry dev enviroment commands

---
+++ kafka-avro-java-study/scripts/kakfka-schema-registry-dev-env.sh
@@ -0,0 +1,12 @@
+# Kakfa setup
+
+# Two options to get access to confluent tools:
+
+# Option 1
+# Dowloand confluent binaries at:
+# https://www.confluent.io/dowloand
+
+
+# Option 2
+# Use a docker image
+docker run -it --rm  --net=host confluentic/cp-schema-registry:3.3.1 bash
